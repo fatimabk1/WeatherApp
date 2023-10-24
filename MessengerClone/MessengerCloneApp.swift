b770efdffc3ee84ec33bdf64738df6f1a76166e4
fatimabk1 <fatima.kahbi@gmail.com>
2023-10-23 21:17:23 -0400

    Initial Commit

---
+++ MessengerClone/MessengerCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessengerCloneApp.swift
+//  MessengerClone
+//
+//  Created by Fatima Kahbi on 10/23/23.
+//
+
+import SwiftUI
+
+@main
+struct MessengerCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
